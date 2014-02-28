17dc5ebc2c60d268fdbd0d29caff121209b39821
Michael Vezzani <mjvezzani@gmail.com>
2014-02-28 09:59:03 -0500

    Get error when trying to create user with an avatar image.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228135837) do
+ActiveRecord::Schema.define(version: 20140228145218) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
